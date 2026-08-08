c070e7b4542f7ca6a72253c38c33dbec3ba59c51
Chandru37-ctrl <smchandru37@gmail.com>
2026-08-08 16:33:34 +0530

    Build initial SyncSpace frontend layout

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="logo">
+        SyncSpace
+      </div>
+
+      <div className="room-info">
+        Workspace
+      </div>
+
+      <div className="user-info">
+        Chandru
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
